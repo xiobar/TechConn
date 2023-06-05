950b11e293a2676c18d7b755fe4760e3f364e1f5
xiobar <yo.yito159@gmail.com>
2023-06-05 17:13:09 -0500

    creacion del fragment de productos e item y datos

--- app/src/main/java/com/gkm/techconn/modelo/ordenesDatos.kt
+++ app/src/main/java/com/gkm/techconn/modelo/ordenesDatos.kt
@@ -1,7 +1,19 @@
 package com.gkm.techconn.modelo
 
-data class ordenesDatos (val nameCliente:String,
-                         val nroOrden:String,
-                         val fecha:String,
-                         val total:String,
-                         val estado:String)
+data class ordenesDatos(
+    val nameCliente: String,
+    val nroOrden: String,
+    val fecha: String,
+    val total: String,
+    val estado: String
+)
+
+data class productos(
+    val descripcion: String,
+    val codigo: String,
+    val marca: String,
+    val empaque: String,
+    val stock: String,
+    val indisce: String,
+    val precio: String
+)
