32b41634512185a2302cf075698fce70b350d4b9
xiobar <george.haz@yahoo.com>
2023-06-07 13:18:14 -0500

    aun falta incorporar las imagenes

--- app/src/main/java/com/gkm/techconn/adapter/productoViewHolder.kt
+++ app/src/main/java/com/gkm/techconn/adapter/productoViewHolder.kt
@@ -1,14 +1,17 @@
 package com.gkm.techconn.adapter
 
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.gkm.techconn.R
 import com.gkm.techconn.databinding.ItemListProductoBinding
 import com.gkm.techconn.modelo.productosDatos
 import com.gkm.techconn.ui.view.FullScreenImageFragment
 import com.squareup.picasso.Picasso
 
 class productoViewHolder(view:View):RecyclerView.ViewHolder(view) {
-    val binding = ItemListProductoBinding.bind(view)
+    private val binding = ItemListProductoBinding.bind(view)
 
     fun render(productoDatosModel:productosDatos){
         binding.txtProDescr.text = productoDatosModel.descripcion
@@ -19,16 +22,22 @@ class productoViewHolder(view:View):RecyclerView.ViewHolder(view) {
         binding.txtProIndice.text = productoDatosModel.indice
         binding.txtProPrecio.text = productoDatosModel.precio
 
-        Picasso.get()
-            .load(productoDatosModel.imagen)
-            .into(binding.imgProView)
+        binding.imgProView.setOnClickListener{
+            val fullImage = FullScreenImageFragment.newInstance(productoDatosModel.imagen)
 
-        binding.imgProView.setOnClickListener {
-            val imagenUrl = productoDatosModel.imagen
+            val manager:FragmentManager? = itemView.context?.let {
+                if(it is Fragment){
+                    it.childFragmentManager
+                }else{
+                    null
+                }
+            }
 
-            val fullScreenImageFragment = FullScreenImageFragment.newInstance(imagenUrl)
-            val fragmentManager = binding.root.context.packageManager
-            val transaccion = fragmentManager.begin
+            manager?.beginTransaction()
+                ?.replace(com.google.android.material.R.id.container, fullImage)
+                ?.addToBackStack(null)
+                ?.commit()
         }
+
     }
 }
--- app/src/main/java/com/gkm/techconn/ui/view/InicioBarActivity.kt
+++ app/src/main/java/com/gkm/techconn/ui/view/InicioBarActivity.kt
@@ -16,67 +16,87 @@ import com.google.android.material.navigation.NavigationView
 class InicioBarActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityInicioBarBinding
-    private lateinit var drawer:DrawerLayout
-    private lateinit var toogle:ActionBarDrawerToggle
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toogle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInicioBarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val toolbar:androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
 
         drawer = binding.drawInicioBar
 
-        toogle = ActionBarDrawerToggle(this,drawer,toolbar,R.string.nav_draw_open, R.string.nav_draw_close)
+        toogle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.nav_draw_open,
+            R.string.nav_draw_close
+        )
         drawer.addDrawerListener(toogle)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
-        val navigationView:NavigationView = binding.navView
-        navigationView.setNavigationItemSelectedListener (this)
+        val navigationView: NavigationView = binding.navView
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {  //Navegador de items menu
-        val fm:FragmentManager= supportFragmentManager  //Controla lso reemplazos del fragment
-        val ft:FragmentTransaction=fm.beginTransaction()  //Controla el como inia el fragment
-        when (item.itemId){
+        val fm: FragmentManager = supportFragmentManager  //Controla lso reemplazos del fragment
+        val ft: FragmentTransaction = fm.beginTransaction()  //Controla el como inia el fragment
+        when (item.itemId) {
             R.id.nav_item_inicio -> {
                 binding.inluye.toolbarMain.title = "Inicio"
-                ft.replace(R.id.content,InicioFragment()).commit()}
+                ft.replace(R.id.content, InicioFragment()).commit()
+            }
+
             R.id.nav_item_ordenes -> {
                 binding.inluye.toolbarMain.title = "Ordenes"
-                ft.replace(R.id.content,OrdenFragment()).commit()}
+                ft.replace(R.id.content, OrdenFragment()).commit()
+            }
+
             R.id.nav_item_nuevo -> {
                 binding.inluye.toolbarMain.title = "Nueva orden"
-                ft.replace(R.id.content,CrearOrdenFragment()).commit()}
+                ft.replace(R.id.content, CrearOrdenFragment()).commit()
+            }
+
             R.id.nav_item_cliente -> {
                 binding.inluye.toolbarMain.title = "Clientes"
-                ft.replace(R.id.content,ClientesFragment()).commit()}
+                ft.replace(R.id.content, ClientesFragment()).commit()
+            }
+
             R.id.nav_item_producto -> {
                 binding.inluye.toolbarMain.title = "Producto"
-                ft.replace(R.id.content,ProductosFragment()).commit()}
+                ft.replace(R.id.content, ProductosFragment()).commit()
+            }
+
             R.id.nav_item_staPed -> {
                 binding.inluye.toolbarMain.title = "Estado Pedido"
-                ft.replace(R.id.content,EstatusFragment()).commit()
+                ft.replace(R.id.content, EstatusFragment()).commit()
             }
+
             R.id.nav_item_fact -> {
                 binding.inluye.toolbarMain.title = "Facturas"
-                ft.replace(R.id.content,FacturasFragment()).commit()
+                ft.replace(R.id.content, FacturasFragment()).commit()
             }
+
             R.id.nav_item_mapa -> {
                 binding.inluye.toolbarMain.title = "Mapa"
-                ft.replace(R.id.content,MapasFragment()).commit()
+                ft.replace(R.id.content, MapasFragment()).commit()
             }
+
             R.id.nav_item_deuda -> {
                 binding.inluye.toolbarMain.title = "Deudas"
-                ft.replace(R.id.content,DeudaFragment()).commit()
+                ft.replace(R.id.content, DeudaFragment()).commit()
             }
+
             R.id.nav_item_settings -> {
                 binding.inluye.toolbarMain.title = "Opciones"
-                ft.replace(R.id.content,OpcionesFragment()).commit()
+                ft.replace(R.id.content, OpcionesFragment()).commit()
             }
         }
         drawer.closeDrawer(GravityCompat.START)
@@ -94,7 +114,7 @@ class InicioBarActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toogle.onOptionsItemSelected(item))
+        if (toogle.onOptionsItemSelected(item))
             return true
         return super.onOptionsItemSelected(item)
     }
